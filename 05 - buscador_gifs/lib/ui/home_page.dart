eabb1b166987304b3599fbf03907bdee6b619002
Wilder Lopes <wilderlopes@MacBook-Pro-de-Wilder.local>
2019-10-04 11:44:23 -0300

    fontes até aula 85

--- 05 - buscador_gifs/lib/ui/home_page.dart
+++ 05 - buscador_gifs/lib/ui/home_page.dart
@@ -37,8 +37,73 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Image.network("https://developers.giphy.com/branch/master/static/header-logo-8974b8ae658f704a5b48a2d039b8ad93.gif"),
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.black,
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(10.0),
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: "Pesquise aqui!",
+                labelStyle: TextStyle(color: Colors.white),
+                border: OutlineInputBorder() 
+              ),
+              style: TextStyle(color: Colors.white, fontSize: 18.0),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Expanded(
+            child: FutureBuilder(
+              future: this._getGifs(),
+              builder: (context, snapshot){
+                switch (snapshot.connectionState) {
+                  case ConnectionState.waiting:
+                  case ConnectionState.none:
+                    return Container(
+                      width: 200.0,
+                      height: 200.0,
+                      alignment: Alignment.center,
+                      child: CircularProgressIndicator(
+                        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                        strokeWidth: 5.0,
+                      ),
+                    );                   
+                  default:
+                    if (snapshot.hasError) return Container();
+                    else return _createGifTable(context, snapshot);
+                }
+              },
+            ),
+          )
+        ],
+      ),
+      );
   }
+}
+
+Widget _createGifTable(BuildContext context, AsyncSnapshot snapshot){
+  return new GridView.builder(
+    padding: EdgeInsets.all(10.0),
+    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+      crossAxisCount: 2,
+      crossAxisSpacing: 10.0,
+      mainAxisSpacing: 10.0
+    ) ,
+    itemCount: snapshot.data["data"].length,
+    itemBuilder: (context, index){
+      return GestureDetector(
+        child: Image.network(
+          snapshot.data["data"][index]["images"]["fixed_height"]["url"],
+          height: 300.0,
+          fit: BoxFit.cover,
+        ),    
+      );
+    },
+  );
 }
