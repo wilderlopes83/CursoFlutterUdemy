befaa4036b33502ba4b7d8ebdc20b011493a9bb0
Wilder Lopes <wilderlopes@MacBook-Pro-de-Wilder.local>
2019-10-15 22:15:47 -0300

    app completo

--- 05 - buscador_gifs/lib/ui/home_page.dart
+++ 05 - buscador_gifs/lib/ui/home_page.dart
@@ -2,6 +2,10 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:share/share.dart';
+import 'package:transparent_image/transparent_image.dart';
+
+import 'gif_page.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -17,7 +21,7 @@ class _HomePageState extends State<HomePage> {
   Future<Map> _getGifs() async{
     http.Response response;
 
-    if (_search == null)
+    if (_search == null || _search.isEmpty)
       response = await http.get("https://api.giphy.com/v1/gifs/trending?api_key=nsXsOJ3NiuZITV4aPO05DSjs1Y7K3uTI&limit=20&rating=G");
     else
       response = await http.get("https://api.giphy.com/v1/gifs/search?api_key=nsXsOJ3NiuZITV4aPO05DSjs1Y7K3uTI&q=$_search&limit=19&offset=$_offset&rating=G&lang=en");
@@ -111,11 +115,19 @@ class _HomePageState extends State<HomePage> {
       itemBuilder: (context, index){
         if (_search == null || index < snapshot.data["data"].length){
           return GestureDetector(
-            child: Image.network(
-              snapshot.data["data"][index]["images"]["fixed_height"]["url"],
+            child: FadeInImage.memoryNetwork(
+              placeholder: kTransparentImage,
+              image: snapshot.data["data"][index]["images"]["fixed_height"]["url"],
               height: 300.0,
               fit: BoxFit.cover,
             ),    
+            onTap: (){
+              Navigator.push(context, 
+                MaterialPageRoute(builder: (context) => GifPage(snapshot.data["data"][index])));
+            },
+            onLongPress: (){
+              Share.share(snapshot.data["data"][index]["images"]["fixed_height"]["url"]);
+            },
           );
         }
         else{
