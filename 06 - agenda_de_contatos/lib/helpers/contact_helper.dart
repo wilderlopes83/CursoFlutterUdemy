3f332964f60f85124ca0423e729051e6f7e1880c
Wilder Lopes <wilderlopes@MacBook-Pro-de-Wilder.local>
2019-10-21 21:31:25 -0300

    inclusao dos fontes da agenda de contatos

---
+++ 06 - agenda_de_contatos/lib/helpers/contact_helper.dart
@@ -0,0 +1,30 @@
+import 'package:sqflite/sqflite.dart';
+
+final String idColumn = "idColumn";
+final String nameColumn = "nameColumn";
+final String emailColumn = "emailColumn";
+final String phoneColumn = "phoneColumn";
+final String imgColumn = "imgColumn";
+
+class ContactHelper{
+
+
+
+}
+
+
+class Contact{
+  int id;
+  String name;
+  String email;
+  String phone;
+  String img;
+
+  Contact.fromMap(Map map){
+    id = map[idColumn];
+    name = map[nameColumn];
+    email = map[emailColumn];
+    phone = map[phoneColumn];
+    img = map[imgColumn];
+  }
+}
